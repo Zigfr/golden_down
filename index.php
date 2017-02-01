1475eeee78042d699000f06ca9e4b5ff54bb0a2f
Zegfried <metropoliten17@mail.ru>
2017-02-01 19:15:21 +0300

    +родительская модель

--- index.php
+++ index.php
@@ -2,5 +2,10 @@
     require __DIR__ .'/autoload.php';
 
     $users = \App\Models\User::findAll();
+
     var_dump($users);
+    echo '<br />';
+    echo '<br />';
+
+   // echo \App\Models\User::$table;
 
