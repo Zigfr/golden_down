677386d39bb2eef5e65731a7f64d70f09c624737
Zegfried <metropoliten17@mail.ru>
2017-02-01 18:13:02 +0300

    +добавили метод execute класса Db()

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
     require __DIR__ .'/autoload.php';
 
-   $data = new \App\Db();
-
-    //var_dump($data);
+    $data = new \App\Db();
+    $sql = 'CREATE TABLE Arting (id SERIAL, cool INT, words TEXT)';
+    $data->execute($sql);
